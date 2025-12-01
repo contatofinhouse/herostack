fe509417840b245fafab24ec9aaa0c6d25cd8641
contatofinhouse <contatofinhouse@gmail.com>
2025-12-01 15:39:57 -0300

    deploy

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://wfgxstdymupgyjrxbasl.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndmZ3hzdGR5bXVwZ3lqcnhiYXNsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQzNDE1MDcsImV4cCI6MjA3OTkxNzUwN30.9vs0uIXiqLzU7f8eiJuOjah9lwCc6eafwStNNXU5x8I';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
